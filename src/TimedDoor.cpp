e5e2610f0d226ad1c77cd74ac378c972b9f8cb08
Astra-chan-desu <lyosha5438@gmail.com>
2025-05-30 13:42:13 +0300

    Startinghere

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,2 +1,47 @@
 // Copyright 2021 GHA Test Team
 #include "TimedDoor.h"
+#include <chrono>
+#include <thread>
+#include <stdexcept>
+
+
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor & door) : door(door) {}
+
+void DoorTimerAdapter::Timeout() {
+    door.throwState();
+}
+
+TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
+    adapter = new DoorTimerAdapter(* this);
+}
+
+bool TimedDoor::isDoorOpened() { 
+    return isOpened;
+}
+
+void TimedDoor::unlock() {
+    isOpened = true;
+}
+
+void TimedDoor::lock() { 
+    isOpened = false;
+}
+
+int TimedDoor::getTimeOut() const {
+    return iTimeout;
+}
+
+void TimedDoor::throwState() {
+    if (isOpened) {
+        throw std::runtime_error("opened");
+    } else throw std::runtime_error("closed");
+}
+
+void Timer::sleep(int millis) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(millis));
+}
+
+void Timer::tregister(int timeout, TimerClient * client) {
+  sleep(timeout);
+  client->Timeout();
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -3,4 +3,82 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <cstdint>
+#include <thread>
 #include "TimedDoor.h"
+
+class MockTimerClient : public TimerClient {
+ public:
+  MOCK_METHOD(void, Timeout, (), (override));
+};
+
+class  ST3_Strakhov : public::testing::Test {
+ protected:
+  TimedDoor * testDoor;
+  MockTimerClient * testMockClient;
+
+  void SetUp() override {
+    testDoor = new TimedDoor(50);
+    testMockClient = new MockTimerClient();
+  }
+
+  void TearDown() override {
+    delete testDoor;
+    delete testMockClient;
+  }
+};
+
+TEST_F(ST3_Strakhov, startState) {
+    EXPECT_FALSE(testDoor.isDoorOpened());
+}
+
+TEST_F(ST3_Strakhov, unlock1) {
+    testDoor.unlock();
+    EXPECT_TRUE(testDoor.isDoorOpened());
+}
+TEST_F(ST3_Strakhov, unlock2) {
+    testDoor.unlock();
+    testDoor.unlock();
+    EXPECT_TRUE(testDoor.isDoorOpened());
+}
+
+TEST_F(ST3_Strakhov, lock1) {
+    testDoor.unlock();
+    testDoor.lock();
+    EXPECT_FALSE(testDoor.isDoorOpened());
+}
+TEST_F(ST3_Strakhov, lock2) {
+    testDoor.unlock();
+    testDoor.lock();
+    testDoor.lock();
+    EXPECT_FALSE(testDoor.isDoorOpened());
+}
+
+
+TEST_F(ST3_Strakhov, throwState1) {
+    testDoor.unlock();
+    EXPECT_THROW(testDoor.throwState(), std::runtime_error);
+
+}
+TEST_F(ST3_Strakhov, throwState2) {
+    testDoor.unlock();
+    testDoor.lock();
+    EXPECT_THROW(testDoor.throwState(), std::runtime_error);
+}
+
+TEST_F(ST3_Strakhov, Timeout) {
+  DoorTimerAdapter testAdapter(* testDoor);
+  testDoor->unlock();
+  EXPECT_THROW(testAdapter.Timeout(), std::runtime_error);
+}
+
+TEST_F(Test, throwState3) {
+  testDoor->unlock();
+  std::this_thread::sleep_for(std::chrono::milliseconds(70));
+  EXPECT_THROW(testDoor->throwState(), std::runtime_error);
+}
+TEST_F(Test, throwState4) {
+  testDoor->unlock();
+  testDoor->unlock();
+  std::this_thread::sleep_for(std::chrono::milliseconds(70));
+  EXPECT_NO_THROW(testDoor->throwState(), std::runtime_error);
+}
